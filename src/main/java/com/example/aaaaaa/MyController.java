b5b6d816692a3b37714c27650d3b905576405bb4
Nukui <kazuki.nukui@g.dreamcareer.co.jp>
2024-05-22 00:14:10 +0900

    add typing...

--- src/main/java/com/example/aaaaaa/MyController.java
+++ src/main/java/com/example/aaaaaa/MyController.java
@@ -3,20 +3,29 @@ package com.example.aaaaaa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class FormController {
+public class MyController {
 
     @Autowired
-    private Typing typingGame;
+    private Typing typing;
+  
 
+    @GetMapping("/")
+    public String index(Model model) {
+    	model.addAttribute("word", typing.getWord());
+    	return "index";
+    }
+    
     @PostMapping("/check")
     public String check(@RequestParam("userInput") String userInput, Model model) {
-        typingGame.checkInput(userInput);
-        model.addAttribute("score", typingGame.getScore());
-        model.addAttribute("miss", typingGame.getMiss());
+        typing.checkInput(userInput);
+        model.addAttribute("score", typing.getScore());
+        model.addAttribute("miss", typing.getMiss());
+        model.addAttribute("word", typing.getWord());
         return "result";
     }
 }
--- src/main/java/com/example/aaaaaa/Typing.java
+++ src/main/java/com/example/aaaaaa/Typing.java
@@ -1,40 +1,52 @@
 package com.example.aaaaaa;
 
-import java.util.Scanner;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class Typing {
-    private String[] words = {"apple", "banana", "orange", "grape", "melon"};
-    private int score = 0;
-    private int miss = 0;
-    private Scanner scanner = new Scanner(System.in);
+//	private String[] words = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "[", "]", "{", "}", "|",
+//			";", ":", "\"", "'" };
+	private String word = "aaaa";
+	private String[] words = { "banana", "apple"};
+	private int score = 0;
+	private int miss = 0;
 
-    public String getRandomWord() {
-        return words[(int) (Math.random() * words.length)];
-    }
+	public String getRandomWord() {
+		return words[(int) (Math.random() * words.length)];
+	}
 
-    public void checkInput(String input) {
-        if (input.equals("q")) {
-            return;
-        }
+	public void checkInput(String input) {
+//	    if (input.equals("q")) {
+//	        return;
+//	    }
+		
+	    String word = getRandomWord();
+	    setWord(word);
+	    
+	    System.out.println(word); // コンソールに表示する代わりに、ビューに表示するためにこの行をコメントアウトします。
 
-        String word = getRandomWord();
-        System.out.println(word); // コンソールに表示する代わりに、ビューに表示するためにこの行をコメントアウトします。
+	    if (input.equals(word)) {
+	        score++; // 入力が正しい場合にスコアをインクリメント
+	    } else {
+	        miss++;
+	    }
+	}
 
-        if (input.equals(word)) {
-            score++;
-        } else {
-            miss++;
-        }
-    }
+	
+	
+	public void setWord(String word) {
+		this.word = word;
+	}
 
-    public int getScore() {
-        return score;
-    }
+	public String getWord() {
+		return word;
+	}
 
-    public int getMiss() {
-        return miss;
-    }
+	public int getScore() {
+		return score;
+	}
+
+	public int getMiss() {
+		return miss;
+	}
 }
